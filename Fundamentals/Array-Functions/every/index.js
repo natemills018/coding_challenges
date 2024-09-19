a4e5807196376ff19ca4c97fe511989688772f9b
Nate Mills <natemills018@gmail.com>
2024-09-18 20:39:57 -0400

    Completing the array-function every with a for loop

--- Fundamentals/Array-Functions/every/index.js
+++ Fundamentals/Array-Functions/every/index.js
@@ -1,16 +1,25 @@
 function every(arr) {
-    // You are given an array of a few different strings.
-    // You must return a boolean on whether or not that array has every string being at least 9 characters or more in length
-    // For example, if arr === ["restaurant", "spaghetti", "antidisestablishmentarianism", "cat"]
-    //    arr.every((word) => word.length >= 9) would return `false` since not all strings match that condition.
-    // Instead of using .every, you must use just a regular for loop.
+  // You are given an array of a few different strings.
+  // You must return a boolean on whether or not that array has every string being at least 9 characters or more in length
+  // For example, if arr === ["restaurant", "spaghetti", "antidisestablishmentarianism", "cat"]
+  //    arr.every((word) => word.length >= 9) would return `false` since not all strings match that condition.
+  // Instead of using .every, you must use just a regular for loop.
 
-    let results = null;
+ 
+  for (let i = 0; i < arr.length; i++) {
 
-    // Define your for loop here
-    // return true or false based on whether or not every string in the array is 9 or more characters
+    if (typeof arr[i] !=='string' || arr[i].length < 9 ) {
+    
+      return false;
+    } 
+  } 
 
-    return results;
+  return true;
+
+  // Define your for loop here
+  // return true or false based on whether or not every string in the array is 9 or more characters
+
+//   return results;
 }
 
 module.exports = every;
